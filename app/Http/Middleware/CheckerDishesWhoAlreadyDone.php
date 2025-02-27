d601da218d12d515baa55e8cb85d89bcad1882b4
cirilocanganjo <cirilocanganjolr@gmail.com>
2025-02-27 22:46:50 +0100

    middleware created

---
+++ app/Http/Middleware/CheckerDishesWhoAlreadyDone.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CheckerDishesWhoAlreadyDone
+{
+   public $orders;
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $this->checkerDishes();
+            return $next($request);
+        }catch(Exception $ex){
+            return back()->with(["error" => $ex->GetMessage()],500);
+        }
+    }
+
+    public function checkerDishes() {
+        try {
+            $this->orders = \App\Models\OrderClient::query()->where("order_status", "finished")->get();
+            if ($this->orders) {
+                foreach ($this->orders as $order) {
+                    $currentTime = \Carbon\Carbon::now();
+                    $hoursDifference = $order->created_at->diffInHours($currentTime);
+                    if ($hoursDifference >= 3) {
+                        DB::beginTransaction();
+                        \App\Models\OrderClient::destroy([$order->id]);
+                        DB::commit();
+                    }
+                }
+            }
+        } catch (Exception $ex) {
+            return back()->with(["error" => $ex->GetMessage()],500);
+        }
+    }
+}
--- routes/client/routes.php
+++ routes/client/routes.php
@@ -5,9 +5,9 @@ use App\Http\Livewire\Client\ClientProfileComponent;
 use App\Http\Livewire\Client\CreateAccountClientComponent;
 use \Illuminate\Support\Facades\Route;
 
-Route::middleware("checkerallreservationstatus")->group( function () {
+Route::middleware(["checkerallreservationstatus"])->group( function () {
 Route::get("/registar.nova.conta", CreateAccountClientComponent::class)->name("client.create.account");
-Route::middleware(['auth'])->group(function () {
+Route::middleware(["checker.dishes.already.finished", "auth"])->group(function () {
 Route::get('/painel/cliente',ClientDashboardComponent::class)->name('prohotel.client.panel.dashboard');
 Route::get("/painel/cliente/perfil/", ClientProfileComponent::class)->name('prohotel.client.panel.profile');
 
